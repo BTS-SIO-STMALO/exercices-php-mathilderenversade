16cdd83649a62e34a157928a809782a4d1aa1dc1
mathilderenversade <mathirenversade@gmail.com>
2020-11-02 16:20:09 +0100

    correction des exos;

--- corrections.php
+++ corrections.php
@@ -6,7 +6,7 @@
    $note_maths = 15;
    $note_francais = 12;
    $note_histoire_geo = 9;
-   $moyenne = 0;
+   $moyenne = ($note_maths+$note_francais+$note_histoire_geo)/3;
    echo 'La moyenne est de '.$moyenne.' / 20.';
 ?>
 
@@ -17,7 +17,7 @@
 <?php
    $prix_ht = 50;
    $tva = 20;
-   $prix_ttc = 0;
+   $prix_ttc = $prix_ht+($prix_ht*$tva/100);
    echo 'Le prix TTC du produit est de '.$prix_ttc.' €.';
 ?>
 
@@ -27,7 +27,12 @@
 
 <!-- Votre code php est à écrire ci-dessous -->
 
+<?php 
 
+$test = '42';
+var_dump($test);
+
+?>
 
 
 
@@ -37,6 +42,32 @@
 
 <!-- Votre code php est à écrire ci-dessous -->
 
+<?php
+$sexe = "homme";
+
+if($sexe =="homme"){
+    echo "Bonjour Monsieur";
+}else if ($sexe=="femme"){
+    echo "Bonjour Madame";
+}else {
+    echo "Bonjour personne non identifiée ou génrée";
+};
+
+// ou bien en utilisant le switch : https://www.php.net/manual/fr/control-structures.switch.php
+
+    switch($sexe){
+        case("homme"):
+            echo "Bonjour Monsieur";
+        break;
+        case("femme"):
+            echo "Bonjour Madame";
+        break;
+        default :
+        echo "Bonjour personne non genrée ou identifiée";
+        break;
+    };
+
+?>
 
 
 
@@ -45,8 +76,24 @@
 <p>Déclarer une variable $age qui contient la valeur de type integer de votre choix. Réaliser une condition pour afficher si la personne est mineure ou majeure.</p>
 
 <!-- Votre code php est à écrire ci-dessous -->
+<?php
 
+$age = 17;
+if($age < 18){
+    echo "mineur";
+} else {
+    echo "majeur";
+};
 
+// autre 
+
+if ($age >= 18) {
+    echo "Vous êtes majeur";
+}else {
+    echo "Vous êtes mineur";
+};
+
+?>
 
 
 <h2>Exercice 6</h2>
@@ -60,7 +107,18 @@ Le bon message s'affichera en fonction des valeurs contenues dans $budget et $ac
 
 <!-- Votre code php est à écrire ci-dessous -->
 
+<?php
 
+$budget = 1553.89;
+$achat = 1554.76;
+
+if($budget >= $achat){
+    echo "Le budget :".$budget."€ permet de payer tous les achats d'un montant de ".$achat. "€!";
+}else {
+    echo "Le budget :".$budget."€  ne permet pas de payer tous les achats d'un montant de ".$achat. "€!"; 
+};
+
+?>
 
 
 
@@ -69,7 +127,25 @@ Le bon message s'affichera en fonction des valeurs contenues dans $budget et $ac
 
 <!-- Votre code php est à écrire ci-dessous -->
 
+<?php 
+$heure = 23.99;
+/*
+J'utilise cette fonction php settype afin de typer ma variable et donc mieux gérer sa valeur de sortie)*https://www.php.net/manual/fr/function.settype.php
+settype($heure,"integer");
+var_dump($heure);
+*/
+echo ($heure>=22 || $heure<6 ? "c'est la nuit":(($heure<14)?"c'est le matin":"c'est l'aprem"));
 
+if ($heure < 0 || $heure > 23){
+    echo "Cette heure est incorrecte";
+}elseif ($heure >= 7 && $heure < 12){
+    echo "Bonne matinée";
+}elseif ($heure >=12 && $heure < 22) {
+    echo "Bonne après-midi";
+}else {
+    echo "Bonne nuit";
+}
+?>
 
 
 
@@ -78,6 +154,23 @@ Le bon message s'affichera en fonction des valeurs contenues dans $budget et $ac
 
 <!-- Votre code php est à écrire ci-dessous -->
 
+<?php 
+
+/*
+$i = 77000;
+while($i < 78000){
+    echo $i++;
+};
+*/
+
+$i = 77000 ;
+
+while ($i <= 77999){
+    echo $i.' ';
+    $i++;
+};
+
+?>
 
 
 
@@ -109,7 +202,23 @@ La présentation du résultat attendues est la suivante :
 
 <!-- Votre code php est à écrire ci-dessous -->
 
+<?php 
 
+for ($i=5; $i<=5; $i++){
+    echo "Table de multiplication de".$i ."<br>";
+    for ($j=1; $j<=10;$j++){
+        echo $i ."x". $j ."=".$i*$j."<br>";
+    }
+};
+
+$nombre = 5;
+
+for ($i=1 ; $i<=10 ; $i++){
+    echo $nombre .' x ' .$i. ' = '.($nombre*$i).'<br/>'; 
+};
+
+
+?>
 
 
 
@@ -131,6 +240,16 @@ La présentation du résultat attendues est la suivante :
 
 <!-- Votre code php est à écrire ci-dessous -->
 
+<?php 
+
+for ($i=1 ; $i <= 5 ; $i++){
+    echo '<br/>';
+    for ($u=1 ; $u<=$i ; $u++){
+        echo $i;
+    };
+};
+echo '<br/>';
+?>
 
 
 
@@ -146,9 +265,22 @@ La présentation du résultat attendues est la suivante :
 
 <!-- Votre code php est à écrire ci-dessous -->
 
+<?php
+
+$k = 0;
+
+while($k < 20){
+    if ($k == 10){
+        echo "<strong>".$k ."</strong>";
+    } else {
+    echo $k;
+    }
+    echo '<br/>';
+    $k = $k+2;
+}
 
 
-
+?>
 
 <h2>Exercice 12</h2>
 
@@ -162,6 +294,21 @@ Afficher les valeurs de tous les éléments du tableau en utilisant la boucle <c
 
 <!-- Votre code php est à écrire ci-dessous -->
 
+<?php 
+
+$tableau = [
+    "France" => "Paris",
+    "Allemagne" => "Berlin",
+    "Italie" => "Rome"
+];
+
+foreach($tableau as $ville){
+    echo $ville .' ';
+};
+
+
+?>
+
 <h2>Exercice bonus</h2>
 <p>En utilisant la fonction <code>rand()</code>, remplir un tableau avec 10 nombres aléatoires. Puis, tester si le chiffre 42 est dans le tableau et afficher un message en conséquence comme par exemple "Le chiffre 42 est bien dans le tableau" ou "Le chiffre 42 n'est pas dans le tableau". Enfin, afficher le contenu de votre tableau avec <code>var_dump</code>.
 <br>
@@ -172,3 +319,33 @@ Le tableau ne contient pas la valeur 42.
 array(10) { [0]=> int(46) [1]=> int(7) [2]=> int(18) [3]=> int(43) [4]=> int(3) [5]=> int(22) [6]=> int(8) [7]=> int(41) [8]=> int(49) [9]=> int(44) }
 </p>
 <!-- Votre code php est à écrire ci-dessous -->
+
+<?php
+
+$table = [];
+//var_dump($table);
+$i = 0;
+
+while($i < 10) {
+    $table[] = rand(0, 50);
+    $i++;
+};
+
+
+if (in_array(42, $table)){
+    echo "Le chiffre 42 est bien dans le tableau";
+} else {
+    echo "Le chiffre 42 n'est pas dans le tableau";
+};
+var_dump($table);
+
+// autre solution
+
+foreach ($table as $nombre){
+    $verif = $nombre == 42 ? true : $verif;
+};
+echo $verif == true ? "Le nombre est bien dans le tableau" : "Le nombre n'est pas dans le tableau";
+
+var_dump($table);
+
+?>
